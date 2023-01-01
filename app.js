689a71de7aa191de51755f52d1e1f2eb13029c50
SrishtiC99 <iamsri9631@gmail.com>
2023-01-01 23:11:38 +0530

    Connected to mongoose

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const date = require(__dirname + "/date.js");
 console.log(date);
 
@@ -9,6 +10,37 @@ app.use(bodyParser.urlencoded({
 }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/todolistDB');
+}
+
+const itemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = new mongoose.model("Item", itemSchema);
+
+const item1 = new Item({
+  name: "Welcome to your ToDo List!"
+});
+const item2 = new Item({
+  name: "Hit this + button to add anew item"
+});
+const item3 = new Item({
+  name: "<-- Hit this to delete an item"
+});
+
+const defeaultItems = [item1, item2, item3];
+Item.insertMany(defeaultItems, function(err){
+  if(err){
+    console.log(err);
+  }else{
+    console.log("Successfully saved defaults item to DB");
+  }
+})
 
 const todoItems = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
